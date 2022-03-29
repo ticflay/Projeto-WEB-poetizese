0e09a9e4a1751d14630d6e92fdc53c9b2efc4224
Samara <sntfreitas@gmail.com>
2022-03-29 15:45:09 -0300

    SignUp

--- src/processes/registrationProcesses.js
+++ src/processes/registrationProcesses.js
@@ -1,3 +1,13 @@
-export const signUp = () => {
-  return null;
+import { normalize, schema } from 'normalizr';
+import { apiPost } from './helpers/api'
+
+const userSchema = new schema.Entity("users");
+
+export const signUp = (values, dispatch, props) => {
+  apiPost("https://poetizese-api.herokuapp.com/users").send(values).then((resp) => {
+    dispatch({
+      type: "USER_CREATED",
+      ...normalize(resp.body, new schema.Array(userSchema))
+    })
+  })
 };
