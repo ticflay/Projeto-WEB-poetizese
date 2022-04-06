d145b1e32c7ee9aeca8b3822a1c8827226493881
Samara <sntfreitas@gmail.com>
2022-04-06 19:26:00 -0300

    add NavBar

---
+++ src/components/Navbar/NavbarLobby.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import { css } from "glamor";
+import { colors } from "../../lib/themeColors";
+
+const container = css({
+  backgroundColor: colors.white,
+});
+
+const styles = {
+  brand: {
+    fontSize: 35,
+    color: colors.purpleDark,
+    fontWeight: "bolder",
+  },
+  link: {
+    marginLeft: 15,
+    color: "black",
+  },
+};
+
+const NavbarLobby = (props) => {
+  return (
+    <Navbar className={container}>
+      <Container>
+        <Navbar.Brand style={styles.brand} href="/*">
+          Poetizise
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link style={styles.link} href="/">
+              Poemas
+            </Nav.Link>
+            <Nav.Link style={styles.link} href="/">
+              Entrar
+            </Nav.Link>
+            <Nav.Link style={styles.link} href="/signup">
+              Cadastrar
+            </Nav.Link>
+            <Nav.Link style={styles.link} href="/literatura">
+              Literatura
+            </Nav.Link>
+            <Nav.Link style={styles.link} href="/about">
+              Sobre Nós
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarLobby;
--- src/components/lobby/LobbyLayout.js
+++ src/components/lobby/LobbyLayout.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { css } from "glamor";
+import { colors } from "../../lib/themeColors";
+import NavbarLobby from "../Navbar/NavbarLobby";
 
 const container = css({
-  backgroundColor: "#A2A2FF",
+  backgroundColor: colors.purple,
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
 });
 
 export default function LobbyLayout(props) {
-  return <div className={container}>{props.children}</div>;
+  return (
+    <div className={container}>
+      <NavbarLobby />
+      {props.children}
+    </div>
+  );
 }
